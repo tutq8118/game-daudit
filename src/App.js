b6a0c4ec95b00618d6be96b5bd009214ee5d10da
Tu Tran <tutq3@fo.com>
2020-10-19 14:48:02 +0700

    generate Serial

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ import './App.scss';
 
 const openNotification  = placement => {
   notification.info({
-    message: 'Chưa đủ công lực',
+    message: 'Chưa nặn đủ thời gian',
     description:
-      'Hãy dí 2s trờ lên nhóe!',
+      'Hãy dí 2s trờ lên nhé!',
     placement,
   });
 };
@@ -27,6 +27,7 @@ function App() {
   const [mode, setMode] = useState('fast');
   const [result, setResult] = useState(undefined);
   const [resultLoading, setResultLoading] = useState(undefined);
+  const [serial, setSerial] = useState(undefined);
 
   function handleChange(value) {
     console.log(`selected ${value}`);
@@ -45,6 +46,8 @@ function App() {
           var randomString  =  radomNumber.toString();
           var fullRandomString = randomString.padStart(8, "0");
           console.log(fullRandomString);
+
+          setSerial(fullRandomString);
         } else {
           openNotification('bottomRight');
           setResult(0);
@@ -66,50 +69,57 @@ function App() {
   return (
     <div className="App">
       <div className="App-header">
-          <h1>Game Đầu Đít</h1>
-          <div className="App-form">
-              <div className="form-group">
-              <Radio.Group name="radiogroup" defaultValue='fast' onChange={handleModePicker}>
-                <Radio value="fast">Chơi nhanh</Radio>
-                <Radio value="freedom">Chơi tự do</Radio>
-              </Radio.Group>
-              </div>
-              {mode === 'fast' && <div className="form-group">
-                <label htmlFor="">Gọi cái:</label>
-                <Select className="form-select" defaultValue={1} style={{ width: 220 }} onChange={handleChange}>
-                  <Option value={1}>Tổng động viên</Option>
-                  <Option value={2}>Tổng 1, 3, 5, 7</Option>
-                  <Option value={3}>Tổng 2, 4, 6, 8</Option>
-                  <Option value={4}>Tổng các số chẵn</Option>
-                  <Option value={5}>Tổng các số lẻ</Option>
-                </Select>
-              </div>}
-              
-              {mode === 'freedom' && <div className="form-group">
-                <label htmlFor="">Chọn tổng các số thứ tự:</label>
-                <Checkbox.Group style={{ width: '100%' }} onChange={handleFreedomPicker}>
-                  <Checkbox value={1}>1</Checkbox>
-                  <Checkbox value={2}>2</Checkbox>
-                  <Checkbox value={3}>3</Checkbox>
-                  <Checkbox value={4}>4</Checkbox>
-                  <Checkbox value={5}>5</Checkbox>
-                  <Checkbox value={6}>6</Checkbox>
-                  <Checkbox value={7}>7</Checkbox>
-                  <Checkbox value={8}>8</Checkbox>
-                </Checkbox.Group>,
-              </div>}
+        <h1>Game Đầu Đít</h1>
+        <div className="App-form">
+          <div className="form-group">
+            <Radio.Group name="radiogroup" defaultValue="fast" onChange={handleModePicker}>
+              <Radio value="fast">Chơi nhanh</Radio>
+              <Radio value="freedom">Chơi tự do</Radio>
+            </Radio.Group>
           </div>
-          <div className="App-money">
-            <img className = "App-money__img"  src = {money} alt = "" />
-            {result !== 1 && <ClickNHold
-              time={2} 
-              onStart={start} 
-              onClickNHold={clickNHold} 
-              onEnd={end} > 
-                <button className = "App-money__btn">Nặn</button>
-            </ClickNHold>}  
-            {result === 1 && <span className = "App-money__serial"></span>}
-          </div>
+          {mode === 'fast' && (
+            <div className="form-group">
+              <label htmlFor="">Gọi cái:</label>
+              <Select className="form-select" defaultValue={1} style={{ width: 220 }} onChange={handleChange}>
+                <Option value={1}>Tổng động viên</Option>
+                <Option value={2}>Tổng 1, 3, 5, 7</Option>
+                <Option value={3}>Tổng 2, 4, 6, 8</Option>
+                <Option value={4}>Tổng các số chẵn</Option>
+                <Option value={5}>Tổng các số lẻ</Option>
+              </Select>
+            </div>
+          )}
+
+          {mode === 'freedom' && (
+            <div className="form-group">
+              <label htmlFor="">Chọn tổng các số thứ tự:</label>
+              <Checkbox.Group style={{ width: '100%' }} onChange={handleFreedomPicker}>
+                <Checkbox value={1}>1</Checkbox>
+                <Checkbox value={2}>2</Checkbox>
+                <Checkbox value={3}>3</Checkbox>
+                <Checkbox value={4}>4</Checkbox>
+                <Checkbox value={5}>5</Checkbox>
+                <Checkbox value={6}>6</Checkbox>
+                <Checkbox value={7}>7</Checkbox>
+                <Checkbox value={8}>8</Checkbox>
+              </Checkbox.Group>
+              ,
+            </div>
+          )}
+        </div>
+        <div className="App-money">
+          <img className="App-money__img" src={money} alt="" />
+          {result !== 1 && (
+            <ClickNHold time={2} onStart={start} onClickNHold={clickNHold} onEnd={end}>
+              <button className="App-money__btn">Nặn</button>
+            </ClickNHold>
+          )}
+          {result === 1 && (
+            <span className="App-money__serial">
+              {serial}
+            </span>
+          )}
+        </div>
       </div>
     </div>
   );
